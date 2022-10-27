c57f933aea4811d19ebf7e34c090e2a268b1061e
Your Nameedwarrojas26 <edrojas843@misena.edu.co>
2022-10-27 04:38:56 -0500

    Cambios en diseño de las vistas

--- rutas/rutas.js
+++ rutas/rutas.js
@@ -1,18 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-  router.get('/', (req, res) => {
-    res.render("index", {titulo: "Edwar Rojas", subtitulo: "Curriculum vitae"});
-  });
-  
-  router.get('/contacto', (req, res) => {
-    res.render("contacto");
-  });
-  
-  router.get("/informacion", (req, res) => {
-    res.render("informacion");
-  });
+router.get("/", (req, res) => {
+  res.render("index", { titulo: "Edwar Rojas", subtitulo: "Curriculum vitae" });
+});
 
-  
+router.get("/contacto", (req, res) => {
+  res.render("contacto", {
+    titulo: "Edwar Rojas",
+    subtitulo: "Curriculum vitae",
+  });
+});
 
-  module.exports = router;
+router.get("/informacion", (req, res) => {
+  res.render("informacion", {
+    titulo: "Edwar Rojas",
+    subtitulo: "Curriculum vitae",
+  });
+});
+
+module.exports = router;
